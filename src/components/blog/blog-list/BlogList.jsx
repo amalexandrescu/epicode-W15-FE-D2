df21f719e426a90455a9fb699376eeb50c3b6458
Alexis <alexandrescu.alexandra.maria@gmail.com>
2023-01-17 16:27:57 +0200

    adds .env and links fe to the be

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
-import posts from "../../../data/posts.json";
+// import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
 
 const BlogList = (props) => {
+  const [posts, setPosts] = useState([]);
+
+  const fetchBlogs = async () => {
+    const apiUrl = process.env.REACT_APP_BE_URL;
+    const resp = await fetch(`${apiUrl}/blogPosts`);
+    const data = await resp.json();
+    setPosts(data);
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
   return (
     <Row>
       {posts.map((post) => (
